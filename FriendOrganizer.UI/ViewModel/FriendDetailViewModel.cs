e4b668ee4c19960e27a34d91b92f5bf067441c90
Gehiler Vargas E <gehiler@hotmail.com>
2019-03-21 11:32:41 -0400

    7. Communicating Between ViewModels

--- FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,7 @@ namespace FriendOrganizer.UI.ViewModel
     public class FriendDetailViewModel : ViewModelBase, IFriendDetailViewModel
     {
         private IFriendDataService _dataService;
+        private IEventAggregator _eventAggregator;
         private Friend _friend;
 
         public Friend Friend
@@ -23,9 +26,17 @@ namespace FriendOrganizer.UI.ViewModel
             }
         }
 
-        public FriendDetailViewModel(IFriendDataService dataService)
+        public FriendDetailViewModel(IFriendDataService dataService, IEventAggregator eventAggregator)
         {
             _dataService = dataService;
+            _eventAggregator = eventAggregator;
+            _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+                .Subscribe(OnOpenFriendDetailView);
+        }
+
+        private async void OnOpenFriendDetailView(int friendId)
+        {
+            await LoadAsync(friendId);
         }
 
         public async Task LoadAsync(int friendId)
